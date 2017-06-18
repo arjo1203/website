7afb0454459f83d6ba08ee1f168caf01a181809d
Jose Araujo <arjo1203@stcloudstate.edu>
2017-06-18 17:06:28 -0500

    Getting ready to deploy

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 var PORT = 8080;
 
-app.use(express.static(__dirname));
+app.use('/', express.static(__dirname + "/app"));
 app.listen(PORT);
 console.log("Server: On " + PORT);
